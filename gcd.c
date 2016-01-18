54dedf208d8aed90c5ab03bdfe395e6933457d77
rokuden <rokuden@sfc.wide.ad.jp>
2016-01-18 23:47:38 +0900

    Update gcd.c

--- gcd.c
+++ gcd.c
@@ -6,26 +6,25 @@ int main(int argc, char *argv[])
 {
  
  int a = 0,b = 0;
- 10 
- 11 if  (argc != 3){
- 12         printf("%s\n",argv[0]);
- 13         return 0;
- 14 }
- 15         a = atoi(argv[1]);
- 16         b = atoi(argv[2]);
- 17         printf("%d\n",atoi(argv[2]));
- 18         printf("GCD = %d\n",gcd(a, b));
- 19         return 0;
- 20 }
- 21 
- 22 int gcd(int x, int y)
- 23 {
- 24         if (y == 0){
- 25                 return x;
- 26                 printf("%d %d", x, y);
- 27         } else {
- 28                 return gcd(y, x % y);
- 29                 printf("%d %d", x, y);
- 30         }
- 31 }
-~                       
+  
+ if  (argc != 3){
+         printf("%s\n",argv[0]);
+         return 0;
+ }
+         a = atoi(argv[1]);
+         b = atoi(argv[2]);
+         printf("%d\n",atoi(argv[2]));
+         printf("GCD = %d\n",gcd(a, b));
+         return 0;
+ }
+ 
+ int gcd(int x, int y)
+ {
+         if (y == 0){
+                 return x;
+                 printf("%d %d", x, y);
+         } else {
+                 return gcd(y, x % y);
+                 printf("%d %d", x, y);
+         }
+ }
